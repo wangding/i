ea93538e1304d2821bb2a7e4b05a82cf44fb8c82
wangding <408542507@qq.com>
2020-10-15 20:45:38 +0800

    chore: modify bsz-page-footer url

--- js/bsz-page-footer.js
+++ js/bsz-page-footer.js
@@ -4,7 +4,7 @@ var $bszPageFooter = (function() {
       + '<footer style="position: absolute; right: 20px; bottom: 10px; font-size: 9pt; color: gray; z-index: 20;">'
         + '<span id="busuanzi_container_site_uv" style="display: inline;">本站访客人数&nbsp;<span id="busuanzi_value_site_uv"></span>&nbsp;人次</span>'
       + '</footer>'
-      + '<script async="" src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>');
+      + '<script async="" src="https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>');
 
   function show(container) {
     var $ctn = $(container),
