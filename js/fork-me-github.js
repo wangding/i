2699e6722f671713151a68dfe94e95bb011c9128
wangding <408542507@qq.com>
2020-11-05 16:47:43 +0800

    chore: use webp

--- js/fork-me-github.js
+++ js/fork-me-github.js
@@ -2,10 +2,10 @@
 var $forkMeGH = (function () {
   var $DOM = $(''
       + '<div class="fork-me-github" style="position: absolute; top: 0; right: 0; z-index: 20;">'
-        + '<a><img src="https://cdn.jsdelivr.net/gh/wangding/sample@master/images/fork-me-on-github-red.png" alt="Fork me on GitHub"></a>'
+        + '<a><img src="https://cdn.jsdelivr.net/gh/wangding/sample@master/images/fork-me-on-github-red.webp" alt="Fork me on GitHub"></a>'
       + '</div>'),
       $arch = $DOM.find('a');
-  
+
   function show(url) {
     $arch.attr('href', url);
     $('body').append($DOM);
