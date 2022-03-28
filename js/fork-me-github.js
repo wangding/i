f6b0ef45ed87127714190cdf393e532832372e0f
wangding <408542507@qq.com>
2022-03-28 08:24:05 +0800

    chore: modify fork-me-github

--- js/fork-me-github.js
+++ js/fork-me-github.js
@@ -1,14 +1,13 @@
 /* exported $forkMeGH */
 var $forkMeGH = (function () {
-  var $DOM = $(''
-      + '<div class="fork-me-github" style="position: absolute; top: 0; right: 0; z-index: 20;">'
-        + '<a><img src="https://cdn.jsdelivr.net/gh/wangding/sample@master/images/fork-me-on-github-red.webp" alt="Fork me on GitHub"></a>'
-      + '</div>'),
-      $arch = $DOM.find('a');
+  //'<div class="fork-me-github" style="position: absolute; top: 0; right: 0; z-index: 20;">'
 
   function show(url) {
-    $arch.attr('href', url);
-    $('body').append($DOM);
+    var $box = document.createElement('div');
+    $box.innerHTML = '<a href="' + url + '"><img src="https://cdn.jsdelivr.net/gh/wangding/sample@master/images/fork-me-on-github-red.webp" alt="Fork me on GitHub"></a>';
+    $box.className = 'fork-me-github';
+    $box.style = 'position: absolute; top: 0; right: 0; z-index: 20;';
+    document.body.append($box);
   }
 
   return {show: show};
