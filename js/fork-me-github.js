e11c78fe7f0227caffd6339b5894601158a0a113
wangding <408542507@qq.com>
2022-07-15 14:16:41 +0800

    chore: fix url

--- js/fork-me-github.js
+++ js/fork-me-github.js
@@ -4,7 +4,7 @@ var $forkMeGH = (function () {
 
   function show(url) {
     var $box = document.createElement('div');
-    $box.innerHTML = '<a href="' + url + '"><img src="https://cdn.jsdelivr.net/gh/wangding/sample@master/images/fork-me-on-github-red.webp" alt="Fork me on GitHub"></a>';
+    $box.innerHTML = '<a href="' + url + '"><img src="http://sample.wangding.co/images/fork-me-on-github-red.webp" alt="Fork me on GitHub"></a>';
     $box.className = 'fork-me-github';
     $box.style = 'position: absolute; top: 0; right: 0; z-index: 20;';
     document.body.append($box);
